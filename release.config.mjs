dd637226032c79373adee212be32007ca6e6bed4
Alex Parra <jparra@occ.com.mx>
2024-08-10 10:28:17 -0600

    ci: Added github actions workflow

--- release.config.mjs
+++ release.config.mjs
@@ -7,6 +7,21 @@ export default {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    '@semantic-release/github'
+    '@semantic-release/github',
+    [
+      '@semantic-release/exec',
+      {
+        prepareCmd: 'npx rjp package.json version ${nextRelease.version}',
+        successCmd: 'echo "Release ${nextRelease.version} published"',
+        failCmd: 'echo "Failed to publish release ${nextRelease.version}"'
+      }
+    ],
+    [
+      '@semantic-release/git',
+      {
+        message: 'ci: Release <%= nextRelease.version %> [skip ci]',
+        assets: ['package.json']
+      }
+    ]
   ]
 }
